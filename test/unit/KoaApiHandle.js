088767a7366a6a4f0f317a58db52ed75f6940d49
Matt Hoyle <code@deployable.co>
2018-01-26 10:27:43 +0000

    Add test coverage

--- test/unit/KoaApiHandle.js
+++ test/unit/KoaApiHandle.js
@@ -7,9 +7,21 @@ describe('mh::test::unit::KoaApiHandle', function(){
     expect( KoaApiHandle ).to.be.ok    
   })
   
-  it('should create a KoaApiHandle', function(){
+  it('should not create a KoaApiHandle', function(){
     let fn = () => new KoaApiHandle()
     expect( fn ).to.throw('No class')
+  })
+
+  it('should return a response function', function(){
+    expect( KoaApiHandle.response() ).to.be.a('function')
+  })
+
+  it('should return a notFound function', function(){
+    expect( KoaApiHandle.notFound() ).to.be.a('function')
+  })
+
+  it('should return an error function', function(){
+    expect( KoaApiHandle.error() ).to.be.a('function')
   })
 
 })
